404c1cd5ede52169b8bc2ae6f31643cea2bc9969
Alsida Dizdari <alsida.dizdari@gmail.com>
2019-01-21 23:36:56 +0100

    Implement simple test

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -3,14 +3,15 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import 'mocha';
 
+
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('API Request', () => {
-  it('should return users', async () => {
+  it('should return User name or language not provided!', async () => {
     return chai.request(app).get('/')
       .then(res => {
-        chai.expect(res.text).to.eql('Hi')
+        chai.expect(res.text).to.eql('User name or language not provided!')
       })
   })
 })
